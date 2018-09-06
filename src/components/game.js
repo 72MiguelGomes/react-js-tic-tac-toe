1d0543ce16dc24c948e77ce392ab71b7c071e44f
Miguel Gomes <jmbg.41@gmail.com>
2018-09-06 13:34:29 +0100

    Implement History Jump function

--- src/components/game.js
+++ src/components/game.js
@@ -8,16 +8,24 @@ class Game extends React.Component {
       history: [{
         squares: Array(9).fill(null),
       }],
+      stepNumber: 0,
       xIsNext: true
     };
   }
 
   ignoreClick(i) {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
 
     return current.squares[i] != null ||
         this.calculateWinner(current.squares) != null;
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
   }
 
   handleClick(i) {
@@ -25,7 +33,7 @@ class Game extends React.Component {
       return
     }
 
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
 
     const squares = current.squares.slice();
@@ -35,6 +43,7 @@ class Game extends React.Component {
       history: history.concat([{
         squares: squares
       }]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext
     })
   }
@@ -62,7 +71,7 @@ class Game extends React.Component {
 
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
 
     const winner = this.calculateWinner(current.squares);
     let status;
@@ -73,7 +82,6 @@ class Game extends React.Component {
     }
 
     const moves = history.map((step, move) => {
-      console.log(JSON.stringify(step));
       const desc = move ?
           'Go to move #' + move :
           'Go to game start';
